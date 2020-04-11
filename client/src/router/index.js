f82e3da37f6f0c3a359c2b2bcfe3dcdf57b619ba
LCY123-cai <2592186051@qq.com>
2020-04-11 23:46:51 +0800

    初步涉及前端---4月10日

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import register from '../views/register.vue'
 
 Vue.use(VueRouter)
 
@@ -17,7 +18,12 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/users/register',
+    name: 'register',
+    component: register
+   }
 ]
 
 const router = new VueRouter({
---
+++ client/src/views/register.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="register-container">
+      <el-form
+          ref="registerFrom"
+          lobel-width="150px"
+          class="register-form"
+          :model="registerFrom"
+          :rules="registerRules"
+        >
+            <el-form-item prop='email' label="邮箱">
+               <el-input v-model="registerForm.email"></el-input>
+            </el-form-item>
+      </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            registerForm: {
+                email: '',
+                password: '',
+                comparePassword: ''
+            },
+            redisterRules: {
+                email: { type: 'email', required: true, message: '请输入有效的邮箱地址' }
+            }
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
